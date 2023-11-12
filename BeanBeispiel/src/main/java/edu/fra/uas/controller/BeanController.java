dca4c28e73d9181537be43c73a69b5249f249698
cp3501 <nawaz3501@gmail.com>
2023-11-12 10:33:32 +0100

    3.Aufgabe (controller+Beancontroller)

---
+++ BeanBeispiel/src/main/java/edu/fra/uas/controller/BeanController.java
@@ -0,0 +1,11 @@
+package edu.fra.uas.controller;
+
+import org.springframework.stereotype.Component;
+//Auch hier wird die Klasse zur Bean instanziiert mithilfe des @Component-Annotation
+@Component
+public class BeanController {
+    
+
+    
+
+}

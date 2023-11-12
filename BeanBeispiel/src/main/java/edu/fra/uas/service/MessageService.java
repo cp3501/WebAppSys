394dd244ce0aed0417fa921f35effd2240dfeef7
cp3501 <nawaz3501@gmail.com>
2023-11-12 10:13:23 +0100

    2. Aufgabe (Klasse: MessageServce)

--- BeanBeispiel/src/main/java/edu/fra/uas/service/MessageService.java
+++ BeanBeispiel/src/main/java/edu/fra/uas/service/MessageService.java
@@ -1,5 +1,22 @@
 package edu.fra.uas.service;
 
+import org.springframework.stereotype.Component;
+//Component muss importiert werden!
+@Component
 public class MessageService {
+    //deklariere message vom type String
+    private String message;
     
+    //public Methode soll jetzt die message returnen mit getMessage
+
+    public String getMessage(){
+        return message;
+    }
+
+
+    //diese Methode soll die message setten also aendern (Getter und Setter- Methoden)!
+    public void setMessage(String message){
+            this.message= message;
+    }
+
 }

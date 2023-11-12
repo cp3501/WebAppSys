c0f938d0df3b2c64b13a8b94d106b3537d785c52
cp3501 <nawaz3501@gmail.com>
2023-11-12 10:38:11 +0100

    3. Aufgabe (Beancontroller)

--- BeanBeispiel/src/main/java/edu/fra/uas/controller/BeanController.java
+++ BeanBeispiel/src/main/java/edu/fra/uas/controller/BeanController.java
@@ -1,11 +1,22 @@
 package edu.fra.uas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //Auch hier wird die Klasse zur Bean instanziiert mithilfe des @Component-Annotation
+
+import edu.fra.uas.service.MessageService;
 @Component
 public class BeanController {
-    
 
+    //in dieser Bean-Klasse wird eine andere Bean (MessageService) injiziert!
+    @Autowired    
+    private MessageService messageService;
+
+    public String putMessage(String message){
+        messageService.setMessage("put message from Beancontroller"+message);
     
+            return messageService.getMessage();
+    }
+
 
 }

2094352df6ed3340c9f73f84019b1eef65cb684d
cp3501 <nawaz3501@gmail.com>
2023-11-12 10:56:02 +0100

    4. Aufgabe, Messageservice mit logging

--- BeanBeispiel/src/main/java/edu/fra/uas/service/MessageService.java
+++ BeanBeispiel/src/main/java/edu/fra/uas/service/MessageService.java
@@ -1,22 +1,30 @@
 package edu.fra.uas.service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+
 //Component muss importiert werden!
 @Component
 public class MessageService {
+    private static final Logger log = LoggerFactory.getLogger(MessageService.class);
+
+
     //deklariere message vom type String
     private String message;
     
     //public Methode soll jetzt die message returnen mit getMessage
 
+  
     public String getMessage(){
+        log.debug(message);
         return message;
     }
 
 
     //diese Methode soll die message setten also aendern (Getter und Setter- Methoden)!
     public void setMessage(String message){
-            this.message= message;
+          log.debug( this.message= message);
     }
 
 }

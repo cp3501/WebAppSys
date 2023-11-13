a023b1a5799545eb97dbfe21a2528ac1e741aa51
cp35 <nawaz3501@gmail.com>
2023-11-13 12:35:08 +0100

    scope auf wert prototype

--- SpringScopes/src/main/java/edu/fra/uas/session/Session.java
+++ SpringScopes/src/main/java/edu/fra/uas/session/Session.java
@@ -5,12 +5,10 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
 
 @Service
-@Scope(value = "singleton") //prototype, singleton, request
+@Scope(value = "prototype") //prototype, singleton, request
 //@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Session {
 

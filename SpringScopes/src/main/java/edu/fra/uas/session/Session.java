627174565592b99e028bc32ba40d461ed2a5a825
cp35 <nawaz3501@gmail.com>
2023-11-13 12:41:52 +0100

    ein- und ausklammern

--- SpringScopes/src/main/java/edu/fra/uas/session/Session.java
+++ SpringScopes/src/main/java/edu/fra/uas/session/Session.java
@@ -5,11 +5,13 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
 @Service
-@Scope(value = "request") //prototype, singleton, request
-//@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+//@Scope(value = "request") //prototype, singleton, request
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Session {
 
 	private static final Logger log = LoggerFactory.getLogger(Session.class);
